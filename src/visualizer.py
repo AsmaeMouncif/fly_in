5ad30010d2d62778272a393152c4e8bf7b1dacde
ASMAE MOUNCIF <158608762+AsmaeMouncif@users.noreply.github.com>
2026-07-16 14:34:02 +0000

    ok

--- src/visualizer.py
+++ src/visualizer.py
@@ -13,23 +13,28 @@ class Visualizer:
         pygame.display.set_caption("fly-in")
         font_small = pygame.font.SysFont(None, 23)
         font_big = pygame.font.SysFont(None, 30)
-        running = True
-        while running:
-            screen.fill((0, 0, 0))
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            for zone in self.graph.zones.values():
-                pygame.draw.circle(screen, zone.color, [zone.x, zone.y], 80, 3)
-                pygame.draw.circle(screen, zone.color, [zone.x, zone.y], 30)
-                if len(zone.name) <= 6:
-                    display_text = zone.name
-                    font = font_small
-                else:
-                    display_text = zone.name[0]
-                    font = font_big
-                text_surface = font.render(display_text, True, (255, 255, 255))
-                text_rect = text_surface.get_rect(center=[zone.x, zone.y])
-                screen.blit(text_surface, text_rect)
-            pygame.display.flip()
-        pygame.quit()
+        try:
+            running = True
+            while running:
+                screen.fill((0, 0, 0))
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                for zone in self.graph.zones.values():
+                    pygame.draw.circle(screen, zone.color, [zone.x, zone.y], 80, 3)
+                    pygame.draw.circle(screen, zone.color, [zone.x, zone.y], 30)
+                    pygame.draw.line(screen, (240, 240, 240), [100, 300], [550, 300], 1)
+                    if len(zone.name) <= 6:
+                        display_text = zone.name
+                        font = font_small
+                    else:
+                        display_text = zone.name[0]
+                        font = font_big
+                    text_surface = font.render(display_text, True, (255, 255, 255))
+                    text_rect = text_surface.get_rect(center=[zone.x, zone.y])
+                    screen.blit(text_surface, text_rect)
+                pygame.display.flip()
+        except KeyboardInterrupt:
+            print("The drones await your command.", end="")
+        finally:
+            pygame.quit()
