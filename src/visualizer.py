f4b5c84077b100b8a3dfc2ae7cb315ceb89cf50c
Asmae Mouncif <asmounci@c3r5p1.1337.ma>
2026-07-23 17:45:35 +0100

    ok

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,7 +1,7 @@
 import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 import pygame
-
+import math
 
 class Visualizer:
     def __init__(self, graph, start_hub_name=None, end_hub_name=None, nb_drones=0, path=None):
@@ -45,6 +45,29 @@ class Visualizer:
         screen_y = self.padding + proportion_y * usable_height
         return int(screen_x), int(screen_y)
 
+    def draw_connections(self, screen, screen_w, screen_h, min_x, max_x, min_y, max_y):
+        for connection in self.graph.connections:
+            zone1 = self.graph.zones[connection.zone1]
+            zone2 = self.graph.zones[connection.zone2]
+            sx1, sy1 = self.to_screen_coords(
+                zone1.x, zone1.y, screen_w, screen_h,
+                min_x, max_x, min_y, max_y
+            )
+            sx2, sy2 = self.to_screen_coords(
+                zone2.x, zone2.y, screen_w, screen_h,
+                min_x, max_x, min_y, max_y
+            )
+            dx = sx2 - sx1
+            dy = sy2 - sy1
+            distance = math.sqrt(dx * dx + dy * dy)
+            if distance == 0:
+                continue
+            #3lines
+            ux, uy = dx / distance, dy / distance
+            start_point = (sx1 + ux * 80, sy1 + uy * 80)
+            end_point = (sx2 - ux * 80, sy2 - uy * 80)
+            pygame.draw.line(screen, (200, 200, 200), start_point, end_point, 1)
+
     def run(self):
         pygame.init()
         screen = pygame.display.set_mode((1100, 600), pygame.RESIZABLE)
@@ -66,6 +89,7 @@ class Visualizer:
                         pygame.image.load("assets/background.png"),
                         (event.w, event.h),
                     )
+            self.draw_connections(screen, screen_w, screen_h, min_x, max_x, min_y, max_y)
             for zone in self.graph.zones.values():
                 sx, sy = self.to_screen_coords(
                     zone.x, zone.y, screen_w, screen_h,
@@ -85,5 +109,10 @@ class Visualizer:
                 text_surface = font.render(display_text, True, (200, 200, 200))
                 text_rect = text_surface.get_rect(center=[sx, sy])
                 screen.blit(text_surface, text_rect)
+                if zone.name not in (self.start_hub_name, self.end_hub_name):
+                    capacity_text = f"0 ⁄ {zone.max_drones}"
+                    capacity_surface = font_small.render(capacity_text, True, (200, 200, 200))
+                    capacity_rect = capacity_surface.get_rect(center=[sx, sy + 110])
+                    screen.blit(capacity_surface, capacity_rect)
             pygame.display.flip()
         pygame.quit()
