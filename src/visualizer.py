7152f58be46f92ef9aa83bcb7456ce6b6baf89b3
Asmae Mouncif <asmounci@c3r5p3.1337.ma>
2026-07-28 14:59:34 +0100

    ok

--- src/visualizer.py
+++ src/visualizer.py
@@ -17,13 +17,13 @@ class Visualizer:
             self.path = path
         else:
             self.path = [start_hub_name]
+        self.last_move_time = 0
         # self.path_index = 0
         # self.padding = 100
         # self.zone_occupancy = {name: 0 for name in self.graph.zones}
         # self.zone_occupancy[start_hub_name] = nb_drones
         # self.drones = [Drone(self.path, drone_id=i) for i in range(self.nb_drones)]
         # self.move_interval = 1500
-        # self.last_move_time = 0
         # self.drone_colors = [random.choice(DRONE_COLORS) for _ in range(nb_drones)]
         # self.propeller_angle = 0
 
@@ -70,28 +70,28 @@ class Visualizer:
     #     screen_y = self.padding + proportion_y * usable_height
     #     return int(screen_x), int(screen_y)
 
-    # def draw_connections(self, screen, screen_w, screen_h, min_x, max_x, min_y, max_y):
-    #     for connection in self.graph.connections:
-    #         zone1 = self.graph.zones[connection.zone1]
-    #         zone2 = self.graph.zones[connection.zone2]
-    #         sx1, sy1 = self.to_screen_coords(
-    #             zone1.x, zone1.y, screen_w, screen_h,
-    #             min_x, max_x, min_y, max_y
-    #         )
-    #         sx2, sy2 = self.to_screen_coords(
-    #             zone2.x, zone2.y, screen_w, screen_h,
-    #             min_x, max_x, min_y, max_y
-    #         )
-    #         dx = sx2 - sx1
-    #         dy = sy2 - sy1
-    #         distance = math.sqrt(dx * dx + dy * dy)
-    #         if distance == 0:
-    #             continue
-    #         #3lines
-    #         ux, uy = dx / distance, dy / distance
-    #         start_point = (sx1 + ux * 80, sy1 + uy * 80)
-    #         end_point = (sx2 - ux * 80, sy2 - uy * 80)
-    #         pygame.draw.line(screen, (200, 200, 200), start_point, end_point, 1)
+    def draw_connections(self, screen, screen_w, screen_h, min_x, max_x, min_y, max_y):
+        for connection in self.graph.connections:
+            zone1 = self.graph.zones[connection.zone1]
+            zone2 = self.graph.zones[connection.zone2]
+            sx1, sy1 = self.to_screen_coords(
+                zone1.x, zone1.y, screen_w, screen_h,
+                min_x, max_x, min_y, max_y
+            )
+            sx2, sy2 = self.to_screen_coords(
+                zone2.x, zone2.y, screen_w, screen_h,
+                min_x, max_x, min_y, max_y
+            )
+            dx = sx2 - sx1
+            dy = sy2 - sy1
+            distance = math.sqrt(dx * dx + dy * dy)
+            if distance == 0:
+                continue
+            #3lines
+            ux, uy = dx / distance, dy / distance
+            start_point = (sx1 + ux * 80, sy1 + uy * 80)
+            end_point = (sx2 - ux * 80, sy2 - uy * 80)
+            pygame.draw.line(screen, (200, 200, 200), start_point, end_point, 1)
 
     # def draw_drones(self, screen, screen_w, screen_h, min_x, max_x, min_y, max_y, font):
     #     drones_by_zone = {}
