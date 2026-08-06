63f14660543913c45f3e02717a36cd0f0174e616
asmounci <asmaemouncif4@gmail.com>
2026-08-06 12:18:11 +0100

    type hint

--- src/drone.py
+++ src/drone.py
@@ -1,15 +1,18 @@
+from typing import List
+
+
 class Drone:
-    def __init__(self, path, drone_id=0):
-        self.path = path
-        self.path_index = 0
-        self.drone_id = drone_id
+    def __init__(self, path: List[str], drone_id: int = 0) -> None:
+        self.path: List[str] = path
+        self.path_index: int = 0
+        self.drone_id: int = drone_id
 
     @property
-    def current_zone(self):
+    def current_zone(self) -> str:
         return self.path[self.path_index]
 
     @property
-    def next_zone(self):
+    def next_zone(self) -> str | None:
         if self.path_index + 1 < len(self.path):
             return self.path[self.path_index + 1]
         return None
